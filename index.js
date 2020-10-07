812d871a2fc2735dda7e145c9dd5d0f39a704eaf
jasonchilcott <jasonchilcottcode@gmail.com>
2020-10-06 23:39:40 -0400

    all deliverables met

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function toggleColor(element) {
     element.style.color = "red"
   }
 }
-header = document.querySelector('#header')
+const header = document.querySelector('#header')
 header.addEventListener('click', function(e){
   const title = e.target
   toggleColor(title)
@@ -47,4 +47,30 @@ header.addEventListener('click', function(e){
 
 /***** Deliverable 2 *****/
 
+const newPlayerForm = document.querySelector('#new-player-form')
+newPlayerForm.addEventListener('submit', function (e){
+  e.preventDefault()
+  const form = e.target
+  const number = form["number"].value
+  const name = form["name"].value
+  const nickname = form["nickname"].value
+  const photo = form ["photo"].value
+
+  const newPlayer = {number: number, name: name, nickname: nickname, photo: photo, likes: 0}
+  renderPlayer(newPlayer)
+})
+
 /***** Deliverable 3 *****/
+const likeButtons = document.querySelectorAll('.like-button')
+for (const button of likeButtons){
+  button.addEventListener('click', function(e){
+    const button = e.target;
+    const parentDiv = button.parentElement;
+    const likesP = parentDiv.querySelector('.likes');
+    const likesA = likesP.textContent.split(" ");
+    let cLikes = parseInt(likesA[0])
+    const newLikes = ++cLikes
+    likesP.textContent = `${newLikes} likes`
+
+  })
+}
